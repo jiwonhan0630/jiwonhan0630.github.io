23bd0c85437377bfc425e42a0db1793165417ac1
한지원 <jiwonhan0630@gmail.com>
2025-12-14 05:29:47 +0900

    헤드룸 라이브러리 적용...안될수도

---
+++ assets/js/headroomsetup.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var myHeader = document.querySelector("#header");
+    if(myHeader) {
+        var headroom = new Headroom(myHeader, {
+            "offset": 100,      // 100px 스크롤하면 반응
+            "tolerance": 5,     // 민감도
+            "classes": {
+                "initial": "animated",
+                "pinned": "headroom--pinned",
+                "unpinned": "headroom--unpinned"
+            }
+        });
+        headroom.init();
+        console.log("Headroom.js finish");
+    } else {
+        console.error("#header not found");
+    }
+
+});
